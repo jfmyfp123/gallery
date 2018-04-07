7f981f7a3564a7b626970507a3b69cbbc140b6d1
jfmyfp123 <1565476233@qq.com>
2018-04-07 12:51:53 +0800

    更新说明

--- showPic/showPic_min.js
+++ showPic/showPic_min.js
@@ -1,55 +1,50 @@
-// 进行文档与javaScript分离
-function prepareGallery(){
-	console.log("11");
-	if(!document.getElementById){
-		return false;
-	}
-	if(!document.getElementsByTagName){
-		retunr false;
-	}
-	if(!document.getElementById("imageGallery")){
-		return false;
-	}
-	var gallery = document.getElementById("imageGallery");
-	var links = gallery.getElementsByTagName("a");
-	console.log("iii");
-	for (var i = 0; i < links.length; i++) {
-		links[i].onclick = function(){
-			return !showPic(this);
-		}
-	}
-}
+// window.onload=function(){  
+//     prepareGallery();  
+// }  
+addLoadEvent(prepareGallery);
 
-// 图片切换函数
-function showPic(whichPic){
-	if(!document.getElementById("placeholder")){
-		return false;
-	}
-	var source = whichPic.getAttribute("href");
-	var placeholder = document.getElementById("placeholder");
-	if(!placeholder.nodeName != "IMG"){
-		return false;
-	}
-	placeholder.setAttribute("src",source);
-	if(document.getElementById("description")){
-		var text = whichPic.getAttribute("title") ? whichPic.getAttribute("title"):" ";
-	    var description = document.getElementById("description");
-	    if(description.firstChild.nodeType == 3){
-	    	description.firstChild.nodeValue = text;
-	    }
-    }
-    return true;
-}
+//结构与行为分离 
+function prepareGallery(){  
+    if(!document.getElementsByTagName) return false;  
+    if(!document.getElementById) return false;  
+    if(!document.getElementById("imageGallery")) return false;  
+    var gallery=document.getElementById("imageGallery");  
+    var links=gallery.getElementsByTagName("a");  
+    for(var i=0;i<links.length;i++){  
+        links[i].onclick=function(){  
+            return !showPic(this);   //取消元素原有默认行为 
+        }  
+    }  
+}  
+  
+function showPic(whichpic){  
+    if(!document.getElementById("placeholder")) return false;  
+    var source=whichpic.getAttribute("href");  
+    var placeholder=document.getElementById("placeholder");  
+    if(placeholder.nodeName!="IMG") return false;  
+    placeholder.setAttribute("src",source);//注意source不要用""  
+    if(!document.getElementById("description")) return true;  
+    var text=whichpic.getAttribute("title")?whichpic.getAttribute("title"):"";  
+    var description=document.getElementById("description");  
+    if(description.firstChild.nodeType==3){  
+        description.firstChild.nodeValue=text;  
+    }  
+    return true;  
+}  
 
-// 
+// 解决每个事件处理函数只能绑定一条指令的问题
+// window.onload = firstFunction;
+//window.onload = secondFunction;
+// 第二个函数会覆盖第一个，使只发生第二个事件行为
 function addLoadEvent(func){
-	var oldonload = window.onload;//原始函数
-	if(typeof window.onload != "function"){
-		window.onload = func;        //若无原始函数，将新函数赋给它
+	var oldonload = window.load;
+	if(typeof window.load != 'function'){
+		window.onload = func;
 	}else{
+		// 创建匿名函数容纳两个函数，再将匿名函数绑定到onload事件上
 		window.onload = function(){
-			oldonload();  			//若有原始函数，将新函数加在原函数后，
-			func();					//用一个匿名函数容纳这两个函数,因为每个事件处理函数只能绑定一条指令
+			oldonload();
+			func();
 		}
 	}
 }
